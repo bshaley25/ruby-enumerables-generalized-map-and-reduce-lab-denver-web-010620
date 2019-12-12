a1544c2c201e5887b025e6b407ab9c49cd4ebd6a
Bradley Haley <bshaley25@gmail.com>
2019-12-12 16:26:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+
+def map(source_array)
+    result = []
+    index = 0 
+    while index < source_array.length
+      result.push(yield(source_array[index])) 
+      index += 1 
+    end
+    result
+end
+
